1bfc9f0c194ad965c61bd080e232848bd59ae1d8
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2017-07-10 19:51:40 -1000

    completed exercise 16

--- exercises.js
+++ exercises.js
@@ -442,6 +442,9 @@ for (var i = 1; i<=5; i++){
 */
   var myFavFoods = ["lemon bar", "carrot cake", "nachos", "bacon cheeseburger", "ramen", "sweet potato fries", "chimichanga"];
 
+for (var i = 0; i<myFavFoods.length; i++){
+	console.log("16.I'm hungry for", myFavFoods[i]);
+}
 
 
 /*
